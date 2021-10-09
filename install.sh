79e536ff62be4a93a6dcb94756e07c939922c9cf
João Cunha <skidrow.jc@gmail.com>
2021-10-09 20:08:48 +0100

    Install scripts and updates

---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -e
+
+installer() {
+  current_dir=`pwd`
+  scripts="${current_dir}/scripts/";
+
+  echo "Installing xorg files..."
+  $1 install -dm 555 "/etc/X11/xorg.conf.d/"
+  $1 install -Dm 644 xorg.conf "/etc/X11/xorg.conf"
+
+  for i in xorg.conf.d/*; do
+    $1 install -Dm 644 "${i}" "/etc/X11/xorg.conf.d/"
+  done
+
+  echo "Installing dotfiles..."
+  install -Dm 600 gitconfig "$HOME/.gitconfig"
+  install -Dm 600 Xresources "$HOME/.Xresources"
+  install -Dm 600 vimrc "$HOME/.vimrc"
+  install -Dm 600 xinitrc "$HOME/.xinitrc"
+  install -Dm 600 zprofile "$HOME/.zprofile"
+
+  echo "Installing scripts..."
+  install -dm 755 "$HOME/.local/scripts/"
+  cd $HOME/.local/
+  ln -s $scripts scripts
+  cd "${current_dir}"
+
+  echo "Coping configurations..."
+  cp -r "${current_dir}"/config/* $HOME/.config/
+  exit 1;
+}
+
+
+
+
+if [ -z $SU ]; then
+  read -p "SU env not set use sudo [y|N]: " val
+  [ "$val" = "Y" ] || [ "$val" = "y" ] && installer "sudo"
+  exit -1;
+fi
+
+installer "$SU"
